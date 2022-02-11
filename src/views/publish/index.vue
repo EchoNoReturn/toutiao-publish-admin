4c72ce7f04b6c70f844b691dd3070c25df16d228
LisEcho-home <2649354585@qq.com>
2022-02-11 20:54:55 +0800

    发布文章细节修改

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -11,30 +11,30 @@
       </div>
       <div>
         <!-- 表单 -->
-        <el-form ref="form" :model="form" label-width="60px">
+        <el-form ref="form" :model="article" label-width="60px">
           <el-form-item label="标题">
-            <el-input v-model="form.name"></el-input>
+            <el-input v-model="article.title"></el-input>
           </el-form-item>
           <el-form-item label="频道">
-            <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-select v-model="article.channels" placeholder="请选择频道">
               <el-option label="区域一" value="shanghai"></el-option>
               <el-option label="区域二" value="beijing"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="内容">
-            <el-input type="textarea" v-model="form.desc"></el-input>
+            <el-input type="textarea" v-model="article.content"></el-input>
           </el-form-item>
           <el-form-item label="封面">
-            <el-radio-group v-model="form.resource">
-              <el-radio label="1">单图</el-radio>
-              <el-radio label="2">两图</el-radio>
-              <el-radio label="3">三图</el-radio>
-              <el-radio label="0">无图</el-radio>
-              <el-radio label="-1">自动</el-radio>
+            <el-radio-group v-model="article.cover.type">
+              <el-radio :label="1">单图</el-radio>
+              <el-radio :label="2">两图</el-radio>
+              <el-radio :label="3">三图</el-radio>
+              <el-radio :label="0">无图</el-radio>
+              <el-radio :label="-1">自动</el-radio>
             </el-radio-group>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit">发布</el-button>
+            <el-button type="primary" @click="onPublish">发布</el-button>
             <el-button>保存草稿</el-button>
           </el-form-item>
         </el-form>
@@ -51,23 +51,13 @@ export default ({
   props: {},
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      },
-      articles: {
+      article: {
         title: '', // 标题
         channels: null, // 频道信息
         content: '', // 内容信息
         cover: {
-          type: 0,
-          img: []
+          img: [], // 封面的图片
+          type: 0 // 封面类型 -1 自动，0 无图，1 单图，2 双图，3 三图
         }
       }
     }
@@ -77,7 +67,7 @@ export default ({
   created () {},
   mounted () {},
   methods: {
-    onSubmit () {
+    onPublish () {
       console.log('submit!')
     }
   }
